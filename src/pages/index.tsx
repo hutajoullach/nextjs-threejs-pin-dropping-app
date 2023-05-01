d925982be79fc61f1bdb32f715286a3ab1071f15
HutaJoullach <hutajoullach@gmail.com>
2023-05-01 20:27:22 +0900

    feat: add auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,16 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
-import { SignIn, SignInButton } from "@clerk/nextjs";
+import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  const user = useUser();
+
+  console.log(user);
 
   return (
     <>
@@ -17,7 +21,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <SignInButton />
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
       </main>
     </>
   );
