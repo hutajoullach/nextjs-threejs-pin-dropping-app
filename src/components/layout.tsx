d36333c6ff4425a55ddb70568318b0f327c69217
HutaJoullach <hutajoullach@gmail.com>
2023-05-02 17:54:26 +0900

    feat: add layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,16 @@
+import type { PropsWithChildren } from "react";
+
+// import theme from "../styles/styles";
+// import Navbar from "./navbar";;
+
+export const PageLayout = (props: PropsWithChildren) => {
+  return (
+    <main className="flex h-screen justify-center">
+      <div className="h-full w-full overflow-y-scroll md:max-w-7xl">
+        {/* <Navbar /> */}
+        {/* <div className={`${theme.h.content} ${theme.top.content}`}> */}
+        <div>{props.children}</div>
+      </div>
+    </main>
+  );
+};
