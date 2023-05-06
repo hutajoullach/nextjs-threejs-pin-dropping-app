cc550482eef1b692905e7a3b4a29a04192bbe4d9
HutaJoullach <hutajoullach@gmail.com>
2023-05-06 15:47:00 +0900

    feat: style navbar

--- src/components/navbar/search.tsx
+++ src/components/navbar/search.tsx
@@ -10,22 +10,25 @@ const Search = () => {
   const [activeSearchTab, setActiveSearchTab] = useState(searchTab.tabCenter);
 
   const toggleSearchTab = useCallback(
-    (hoveredTab: string) => {
-      setActiveSearchTab(hoveredTab);
+    (hoveredTab: string, mouseEvent: string) => {
+      if (mouseEvent === "enter") setActiveSearchTab(hoveredTab);
+
+      if (mouseEvent === "leave") setActiveSearchTab("");
     },
     [setActiveSearchTab]
   );
 
   return (
     <div className="w-full cursor-pointer rounded-full border-[1px] bg-white px-1 py-[3px] shadow-sm transition hover:shadow-md md:w-auto">
-      <ul className="flex list-none flex-row items-center justify-between">
+      <ul className="flex list-none flex-row items-center justify-between gap-2">
         <li
-          onMouseOver={() => toggleSearchTab(searchTab.tabLeft)}
+          onMouseOver={() => toggleSearchTab(searchTab.tabLeft, "enter")}
+          onMouseLeave={() => toggleSearchTab(searchTab.tabLeft, "leave")}
           className="text-sm text-gray-600"
         >
           <div className="group flex flex-row rounded-full bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 hover:text-white group-hover:from-purple-600 group-hover:to-blue-500">
             <div className="flex flex-row gap-1 rounded-full bg-white px-2 py-1 transition-all duration-75 ease-in group-hover:bg-opacity-0">
-              <div className="rounded-full bg-rose-500 p-1 text-white">
+              <div className="rounded-full bg-slate-300 p-1 text-white">
                 <BiSearch size={18} />
               </div>
               <span className="flex items-center justify-center font-semibold">
@@ -34,8 +37,10 @@ const Search = () => {
             </div>
           </div>
           {activeSearchTab === searchTab.tabLeft && (
-            <div className="absolute top-[47px] overflow-hidden rounded-xl bg-white text-sm shadow-md">
-              <SearchMenu searchMenuList={searchMenuList} />
+            <div className="absolute left-1/2 top-[36px] -translate-x-1/2 transform bg-transparent p-3">
+              <div className="overflow-hidden rounded-xl bg-white text-sm shadow-md">
+                <SearchMenu searchMenuList={searchMenuList} />
+              </div>
             </div>
           )}
         </li>
@@ -44,9 +49,9 @@ const Search = () => {
           onMouseOver={() => toggleSearchTab(searchTab.tabCenter)}
           className="text-sm text-gray-600"
         >
-          <div className="group flex flex-row rounded-full bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 hover:text-white group-hover:from-purple-600 group-hover:to-blue-500">
+          <div className="group flex flex-row rounded-full bg-gradient-to-br from-green-400 to-blue-600 p-0.5 hover:text-white group-hover:from-green-400 group-hover:to-blue-600">
             <div className="flex flex-row gap-1 rounded-full bg-white px-2 py-1 transition-all duration-75 ease-in group-hover:bg-opacity-0">
-              <div className="rounded-full bg-rose-500 p-1 text-white">
+              <div className="rounded-full bg-slate-300 p-1 text-white">
                 <BiSearch size={18} />
               </div>
               <span className="flex items-center justify-center font-semibold">
@@ -63,14 +68,18 @@ const Search = () => {
 
         <li
           onMouseOver={() => toggleSearchTab(searchTab.tabRight)}
-          className="flex flex-1 flex-row items-center gap-2 pl-5 text-sm text-gray-600"
+          className="text-sm text-gray-600"
         >
-          <div className="rounded-full bg-rose-500 p-1 text-white">
-            <BiSearch size={23} />
+          <div className="group flex flex-row rounded-full bg-gradient-to-br from-teal-300 to-lime-300 p-0.5 group-hover:from-teal-300 group-hover:to-lime-300">
+            <div className="flex flex-row gap-1 rounded-full bg-white px-2 py-1 transition-all duration-75 ease-in group-hover:bg-opacity-0">
+              <div className="rounded-full bg-slate-300 p-1 text-white">
+                <BiSearch size={18} />
+              </div>
+              <span className="flex items-center justify-center font-semibold">
+                {searchTab.tabRight}
+              </span>
+            </div>
           </div>
-          <span className="hidden font-semibold sm:block">
-            {searchTab.tabRight}
-          </span>
           {activeSearchTab === searchTab.tabRight && (
             <div className="absolute top-[47px] overflow-hidden rounded-xl bg-white text-sm shadow-md">
               <SearchMenu searchMenuList={searchMenuList} />
