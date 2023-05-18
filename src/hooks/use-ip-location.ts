b72a5c9a35f568fbc92e60d144a08d879d09df83
HutaJoullach <hutajoullach@gmail.com>
2023-05-18 23:41:24 +0900

    feat: add ip-api-geocode type

--- src/hooks/use-ip-location.ts
+++ src/hooks/use-ip-location.ts
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 
+import { IPApiGeocode } from "../types/ip-api-geocode";
+
 import axios, { AxiosResponse } from "axios";
 
 const useIPLocation = () => {
-  const [ipLocCoords, setIpLocCoords] = useState();
+  const [ipLocStatus, setIPLocStatus] = useState("");
+  const [ipLocCoords, setIPLocCoords] = useState<IPApiGeocode>();
 
   const ipAPIUrl = "http://ip-api.com/json/?fields=61439";
 
-  axios.get(ipAPIUrl).then((res: AxiosResponse) => {
-    console.log(res);
-    setIpLocCoords(res.data);
+  axios.get(ipAPIUrl).then((res: AxiosResponse<IPApiGeocode>) => {
+    setIPLocStatus("success");
+    setIPLocCoords(res.data);
   });
 
-  return { ipLocCoords };
+  return { ipLocStatus, ipLocCoords };
 };
 
 export default useIPLocation;
---
+++ src/types/ip-api-geocode.ts
@@ -0,0 +1,17 @@
+// IPApiGeocode interface
+export interface IPApiGeocode {
+  status: string;
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  isp: string;
+  org: string;
+  as: string;
+  query: string;
+}
